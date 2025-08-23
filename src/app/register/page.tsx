91b5d33c98b6dd497531d207acf859d8b9b0d404
marcos123123 <snoop4374@gmail.com>
2025-08-23 16:01:05 -0300

    asdasdadsdas

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,9 +3,15 @@ import React from "react";
 const Register = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div className="p-4 mx-auto max-w-md flex justify-center items-center border-2 border-cyan-400/50 rounded-md">
+      <div className="p-4 mx-auto max-w-md flex flex-col gap-4 justify-center items-center border-2 border-cyan-400/50 rounded-md">
         <h1 className="text-white">Register</h1>
-        <form></form>
+        <form> 
+          <label className="text-white">Email:</label><br></br>
+          <input className="border-2 rounded-md border-cyan-400/50" type="text" id="name" name="name"></input><br></br>
+          <label className="text-white">Password:</label><br></br>
+          <input className="border-2 rounded-md border-cyan-400/50" type="text" id="password" name="password"></input><br></br>
+          <input className="text-white" type="submit" value="Register"></input>
+        </form>
       </div>
     </div>
   );
