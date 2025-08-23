ae18d57f63386c364c275528eee74391bac4007d
18juba <jubalucas05@gmail.com>
2025-08-23 15:27:26 -0300

    mp solution

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="px-6 py-3 bg-slate-900 border-b border-white/20 shadow text-white flex justify-between">
-        <h1 className=" text-3xl">MP Soluções</h1>
+        <h1 className=" text-3xl">MP Solutions</h1>
         <nav className="flex items-center mx-auto">
             <ul className="flex gap-8 ">
                 <li className="cursor-pointer transition-colors duration-150 hover:text-cyan-400">Rain</li>
