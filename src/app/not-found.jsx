84f98bc1f5710cfd25a9ffa8b4d4ae9a77890d3f
18juba <jubalucas05@gmail.com>
2025-09-14 16:37:14 -0300

    Not Found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,9 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import Header from "../../components/layout/Header";
-import { getLabel } from "../../../langhandler";
-import { sysStore } from '@/stores/sys';
+import Header from "../components/layout/Header";
+import { getLabel } from "../../langhandler";
+import { sysStore } from "../stores/sys";
 import { useEffect, useState } from 'react';
 
 
