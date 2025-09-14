2776d782b2d77e1a8533ee20f7066cdeb3f2ee7d
18juba <jubalucas05@gmail.com>
2025-09-14 12:30:29 -0300

    Desenhando layout home

--- src/app/home/Cards.tsx
+++ src/app/home/Cards.tsx
@@ -1,54 +1,180 @@
-
 "use client";
 import Image from "next/image";
 
 const ICONS = {
-    humidity: "/icons/icon_humidity.svg",
-    temperature: "/icons/icon_temperature.svg",
+  humidity: "/icons/icon_humidity.svg",
+  temperature: "/icons/icon_temperature.svg",
 };
-
 
 import { TemperaturesMessages } from "@/components/temperatures/temperaturesMessages";
 import { Card } from "@/components/ui/card";
 import { UserStore } from "@/stores/user";
 
 export function TemperatureCard({ temperature }: { temperature: number }) {
-    const { city, state } = UserStore();
+  const { city, state } = UserStore();
 
-    return (
-        <Card style={{ backgroundColor: 'rgba(55, 36, 100, 0.43)', minWidth: 'fit-content' }} className="temperature-card col-span-12 md:col-span-12 lg:col-span-6 px-6 py-6 rounded-lg shadow-lg backdrop-blur-md border border-white/20">
-            <h5 className="text-white text-3xl font-bold flex md:flex-row flex-col justify-between items-center ">
-                <div className="flex items-center gap-2">
-                    <Image src={ICONS.temperature} alt="Temperature icon" width={32} height={32} />
-                    {temperature}°C
-                </div>
-                <div className="flex flex-col justify-center items-center">  
-                    <span>{city}, {state.abreviation}</span>
-                    <span className="text-1x1 w-full items-center climate-info">Nublado</span>
-                </div>
-            </h5>
-            <p className="text-white text-lg mt-4"><TemperaturesMessages temperature={temperature} /></p>
-        </Card>
-    )
+  return (
+    <Card
+      style={{ minWidth: "fit-content" }}
+      className="bg-transparent backdrop-blur-md col-span-12 md:col-span-12 lg:col-span-6 p-5 rounded-lg shadow-md shadow-black/80 border-2 border-white"
+    >
+      <h5 className="text-white flex md:flex-row flex-col justify-between items-center ">
+        <div className="flex items-center gap-4">
+          <Image
+            src={ICONS.temperature}
+            alt="Temperature icon"
+            width={45}
+            height={45}
+          />
+          <p className="text-cyan-400 text-4xl font-bold">{temperature}°C</p>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <span className="text-3xl font-bold">
+            {city}, {state.abreviation}
+          </span>
+          <span className="bg-slate-200/15 rounded w-full items-center text-center text-sm text-slate-200">
+            Nublado
+          </span>
+        </div>
+      </h5>
+      <p className="text-white mt-2">
+        <TemperaturesMessages temperature={temperature} />
+      </p>
+    </Card>
+  );
 }
 
-
 export function UmidityCard({ humidity }: { humidity: number }) {
-    const { city, state } = UserStore();
-    return (
-        <Card style={{ backgroundColor: 'rgba(55, 36, 100, 0.43)', minWidth: 'fit-content' }} className="humidity-card col-span-12 md:col-span-12 lg:col-span-6 px-6 py-6 rounded-lg shadow-lg backdrop-blur-md border border-white/20">
-            <h5 className="text-white text-3xl font-bold flex md:flex-row flex-col justify-between items-center ">
-                <div className="flex items-center gap-2">
-                    <Image src={ICONS.humidity} alt="Humidity icon" width={32} height={32} />
-                    Humidade e conforto
-                </div>
-            </h5>
-            <div>
-                <p className="text-white text-lg mt-4">Umidade do ar</p>
-                <Card style={{ backgroundColor: 'rgba(217, 217, 217, 0.1)' }} className="climate-info p-2 mt-2">
-                    {humidity}%
-                </Card>
-            </div>
-        </Card>
-    )
+  const { city, state } = UserStore();
+  return (
+    <Card
+      style={{ minWidth: "fit-content" }}
+      className="bg-transparent backdrop-blur-md col-span-12 md:col-span-12 lg:col-span-6 p-6 rounded-lg shadow-md shadow-black/80 border-2 border-white"
+    >
+      <h5 className="text-white text-3xl font-bold flex md:flex-row flex-col justify-between items-center ">
+        <div className="flex items-center gap-2">
+          <Image
+            src={ICONS.humidity}
+            alt="Humidity icon"
+            width={45}
+            height={45}
+          />
+          Humidade e conforto
+        </div>
+      </h5>
+      <div className="flex justify-between gap-4">
+        <div className="max-w-8/12">
+          <p className="text-white text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book
+          </p>
+        </div>
+        <div className="bg-sky-300/10 rounded-sm border-2 border-white p-4 pb-0 w-fit text-center my-8">
+          <p className="text-white text-xl font-bold">Índice U(%)</p>
+          <Card className="bg-slate-200/15 rounded border-none text-slate-200 text-sm w-fit py-0.5 px-2">
+            Umidade do Ar
+          </Card>
+          <h1 className="text-cyan-400 text-4xl font-bold py-6">{humidity}%</h1>
+        </div>
+      </div>
+    </Card>
+  );
+}
+
+export function CloudCard() {
+  return (
+    <Card
+      style={{ minWidth: "fit-content" }}
+      className="bg-transparent backdrop-blur-md p-5 rounded-lg shadow-md shadow-black/80 border-2 border-white"
+    >
+      <h5 className="text-white text-3xl font-bold flex md:flex-row flex-col justify-between items-center ">
+        <div className="flex items-center gap-2">
+          <Image
+            src={ICONS.humidity}
+            alt="Humidity icon"
+            width={45}
+            height={45}
+          />
+          <Image
+            src={ICONS.humidity}
+            alt="Humidity icon"
+            width={45}
+            height={45}
+          />
+          Nuvens e Precipitação
+        </div>
+      </h5>
+      <p className="text-white text-justify">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book
+      </p>
+    </Card>
+  );
+}
+
+export function ActivityCard() {
+  return (
+    <Card
+      style={{ minWidth: "fit-content" }}
+      className="bg-transparent backdrop-blur-md p-5 rounded-lg shadow-md shadow-black/80 border-2 border-white"
+    >
+      <h5 className="text-white text-3xl font-bold flex md:flex-row flex-col justify-between items-center ">
+        <div className="flex items-center gap-2">
+          <Image
+            src={ICONS.humidity}
+            alt="Humidity icon"
+            width={45}
+            height={45}
+          />
+          Atividades Recomendadas
+        </div>
+      </h5>
+    </Card>
+  );
+}
+
+export function WindCard() {
+  return (
+    <Card
+      style={{ minWidth: "fit-content" }}
+      className="bg-transparent backdrop-blur-md p-5 rounded-lg shadow-md shadow-black/80 border-2 border-white"
+    >
+      <h5 className="text-white text-3xl font-bold flex md:flex-row flex-col justify-between items-center ">
+        <div className="flex items-center gap-2">
+          <Image
+            src={ICONS.humidity}
+            alt="Humidity icon"
+            width={45}
+            height={45}
+          />
+          Ventos
+        </div>
+      </h5>
+    </Card>
+  );
+}
+
+export function UVCard() {
+  return (
+    <Card
+      style={{ minWidth: "fit-content" }}
+      className="bg-transparent backdrop-blur-md p-5 rounded-lg shadow-md shadow-black/80 border-2 border-white"
+    >
+      <h5 className="text-white text-3xl font-bold flex md:flex-row flex-col justify-between items-center ">
+        <div className="flex items-center gap-2">
+          <Image
+            src={ICONS.humidity}
+            alt="Humidity icon"
+            width={45}
+            height={45}
+          />
+          UV Index
+        </div>
+      </h5>
+    </Card>
+  );
 }
