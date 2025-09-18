b51d03244aee77464b24a2053632d768004217d6
Jhon171819 <jonathancosta1717@gmail.com>
2025-09-17 22:04:56 -0300

    feat(routes): users routes

---
+++ api/apiV1.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+export const dedicatedApit = (route: string) => ({
+    get: async (endpoint: string) => {
+        return api.get(`/${route}${endpoint}`)
+    },
+    put: async (endpoint: string, body: object) => {
+        return api.put(`/${route}${endpoint}`, body)
+    },
+    post: async (endpoint: string, body: object) => {
+        return api.post(`/${route}${endpoint}`, body)
+    },
+    delete: async (endpoint: string) => {
+        return api.delete(`/${route}${endpoint}`)
+    }
+})
+
---
+++ api/routes/users/index.ts
@@ -0,0 +1,16 @@
+import { api, dedicatedApit } from "../../apiV1"
+
+export const userApi = dedicatedApit("users")
+
+
+export const login = async ({nome, senha}: {nome: string, senha: string}) => {
+    return api.post("/login", {nome, senha})
+}
+
+export const register = async ({nome, senha}: {nome: string, senha: string}) => {
+    return api.post("/registro", {nome, senha})
+}
+
+export const updateAddress = async ({latitude,  longitude}: {latitude: string, longitude: string}) => {
+    return userApi.put("/atualizar_endereco", {latitude, longitude})
+}
